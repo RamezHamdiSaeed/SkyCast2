1f2f704ff3e26cab2c51389d84b6e3d2efb716d3
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-05-12 13:42:14 -0700

    Initial Commit

---
+++ SkyCast2/SkyCast2App.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCast2App.swift
+//  SkyCast2
+//
+//  Created by Ramez Hamdi Saeed on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkyCast2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
